fe6a3c1773ace497287a50d8c9427d561c3b2b5d
catnguyen90 <catnguyen90@gmail.com>
2022-02-15 22:06:06 -0500

    Render all the notes inside notes.js as a separate Note component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Heading />
-      <Note />
+      {notes.map((notesEntry) => (
+        <Note
+          key={notesEntry.key}
+          title={notesEntry.title}
+          content={notesEntry.content}
+        />
+      ))}
       <Footer />
     </div>
   );
